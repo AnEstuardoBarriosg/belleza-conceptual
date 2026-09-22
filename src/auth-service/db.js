79060522ed05c847e3621d894b1eb2fb5e654139
Angel_B <correomarvelsrivals@gmail.com>
2026-09-21 21:27:26 -0600

    Prepare services for Azure environment

--- src/auth-service/db.js
+++ src/auth-service/db.js
@@ -5,7 +5,11 @@ const pool = new Pool({
   port: Number(process.env.DB_PORT || 5432),
   database: process.env.DB_NAME || "belleza_conceptual",
   user: process.env.DB_USER || "bc_app",
-  password: process.env.DB_PASSWORD || ""
+  password: process.env.DB_PASSWORD || "",
+  ssl:
+    process.env.DB_SSL === "true"
+      ? { rejectUnauthorized: false }
+      : false
 });
 
 module.exports = pool;
